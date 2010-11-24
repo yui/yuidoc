3ebaa95ed85ff86bd1ea97e1b051534bc925db76
Adam Moore <adamoore@yahoo-inc.com>
2010-11-24 11:13:09 -0800

    Initial revision.

---
+++ README.md
@@ -0,0 +1,10 @@
+
+Updated yuidoc parser, written in js -- early work in progress
+
+Usage:
+lib/yuidoc.js ../yui3/src
+
+This will produce a data structure in out/data.json by default.
+
+Random notes below.
+
---
+++ test/test.js
@@ -0,0 +1,48 @@
+/**
+ * The module
+ * @module mymodule
+ * @category one,two
+ * @category three
+ * @requires one
+ * @requires two
+ * @uses three
+ * @uses four
+ */
+
+/**
+ * The submodule
+ * @submodule mysubmodule
+ * @category three,four
+ */
+
+
+/**
+ * The class def
+ * @class myclass
+ * @constructor
+ */
+
+/**
+ * test optional
+ * @method testoptional
+ * @param notype my desc
+ * @param {int} namesecond my desc
+ * @param namefirst {string} my desc
+ * @param [optionalvar] {bool} my desc
+ * @param {string} [optionalwithdefault="defaultval"] my desc
+ * @returns something without a type
+ * @example
+ *   This is code
+ * @example
+ *   This is more code
+ */
+
+/**
+ * test object param
+ * @method testobjectparam
+ * @param {object} anobject the object
+ * @param {string} anobject.prop1 prop1
+ * @param {bool} anobject.prop2 prop2
+ * @return {string} something with a type
+ */
+
