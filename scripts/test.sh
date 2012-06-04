f5ef71c5f04e0b149fad767d77b66b09c5201804
Dav Glass <davglass@gmail.com>
2012-06-04 13:09:51 -0500

    Added symlink to tests, tests will fail if this does not parse properly

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,6 +5,15 @@ cd "$(dirname "$0")"
 ./prep.sh
 cd ../tests/
 wait
+cd ./input
+if [ -L ./test-linked ]; then
+    rm ./test-linked;
+fi
+ln -sf ./test2 ./test-linked
+wait
+cd ../
+wait
 ../node_modules/.bin/yuitest ./parser.js ./builder.js ./options.js
 
 exit $?
+
